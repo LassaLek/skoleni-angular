b6f09b8539b066ba40d5c1f4fff4f2535ba81d99
lassal <lassal@seznam.cz>
2019-08-20 15:02:18 +0200

    add completed @Output communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'skoleni';
+  clickCount = 0;
+
+  increment() {
+    this.clickCount++;
+  }
 }
--- src/app/feature/components/list/list.component.ts
+++ src/app/feature/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -6,11 +6,17 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-  @Input() item: string = 'DEFAULT';
+  // can be bound to different name
+  @Input(/*'list-item'*/) item: string = 'DEFAULT';
+  @Output() clickEmitter = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick() {
+    this.clickEmitter.emit()
   }
 
 }
