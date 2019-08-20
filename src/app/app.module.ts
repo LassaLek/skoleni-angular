d476218877dfad16a4ae686ea962ceb435a617c8
lassal <lassal@seznam.cz>
2019-08-20 13:55:01 +0200

    introduce feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FeatureModule } from './feature/feature.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FeatureModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
