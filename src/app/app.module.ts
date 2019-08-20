06eda399d1f81666c784606f69614e487062753d
lassal <lassal@seznam.cz>
2019-08-20 14:34:12 +0200

    complete component examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FeatureModule } from './feature/feature.module';
 import { BoxInlineComponent } from './components/box-inline/box-inline.component';
+import { BoxComponent } from './components/box/box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoxInlineComponent
+    BoxInlineComponent,
+    BoxComponent
   ],
   imports: [
     BrowserModule,
